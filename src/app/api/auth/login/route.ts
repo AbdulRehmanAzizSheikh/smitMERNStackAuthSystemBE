84fafcc49f97bde455f115766396c1e563c78683
abdulrehmanazizf5 <abdulrehmanaziz5f@gmail.com>
2026-04-23 21:09:25 +0500

    feat: implement auth system with login, logout, and user retrieval API routes using jsonwebtoken

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import { connectMongodb } from "@/lib/db/connectMongodb";
+import User from "@/lib/models/User";
+import jwt from "jsonwebtoken";
+
+export async function POST(req: Request) {
+  await connectMongodb();
+  const body = await req.json();
+  const { email, password } = body;
+
+  if (!email || !password) {
+    return NextResponse.json(
+      { message: "All fields are required!" },
+      { status: 400 },
+    );
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if(!user){
+      return NextResponse.json(
+        { message: "Invalid credentials!" },
+        { status: 404 },
+      );
+    }
+    if(user.password != password){
+      return NextResponse.json(
+        { message: "Invalid credentials!" },
+        { status: 401 },
+      );
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    return NextResponse.json(
+      { message: "User login successfully!", token: token },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "User login failed!", error: error },
+      { status: 500 },
+    );
+  }
+}
